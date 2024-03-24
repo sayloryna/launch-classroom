e73230f78054ccd9d7314c89c6498ce1694df722
Mario González <mario@mariogl.com>
2024-03-24 14:32:56 +0100

    Initial commit

---
+++ src/components/header.ts
@@ -0,0 +1,39 @@
+export const renderHeader = () => {
+  const header = document.createElement("header");
+
+  const currentUrl = new URL(window.location.href);
+
+  header.innerHTML = `
+    <header class="main-header">
+      <div class="container">
+        <h1>Classroom</h1>
+        <nav class="main-navigation">
+          <ul>
+            <li>
+              <a href="/estudiantes"${
+                currentUrl.pathname === "/estudiantes" ? " class='active'" : ""
+              }>Estudiantes</a>
+            </li>
+            <li>
+              <a href="/cursos"${
+                currentUrl.pathname === "/cursos" ? " class='active'" : ""
+              }>Cursos</a>
+            </li>
+            <li>
+              <a href="/notas"${
+                currentUrl.pathname === "/notas" ? " class='active'" : ""
+              }>Notas</a>
+            </li>
+            <li>
+              <a href="/estadisticas"${
+                currentUrl.pathname === "/estadisticas" ? " class='active'" : ""
+              }>Estadísticas</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  `;
+
+  document.body.prepend(header);
+};
