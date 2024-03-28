52a0bb3f42cc6ec73b1a6a2ab84f9574febcca09
Clara <clarafdezfraga@gmail.com>
2024-03-28 17:22:59 +0100

    Fix double repetd code in getGradesStats

--- src/stats/service/statsService.ts
+++ src/stats/service/statsService.ts
@@ -5,30 +5,35 @@ export const getCourseStats = (courseId: number): CourseStats => {
   const courseGrades: Grade[] = grades.filter(
     (grade) => grade.courseId === courseId
   );
+
   const courseGradesValues: number[] = courseGrades.map((grade) => grade.value);
   const sortedByValueCourseGrades = courseGrades.toSorted(
     (gradeA, gradeB) => gradeB.value - gradeA.value
   );
 
+  const passedGradesTotal: number = courseGrades.filter(
+    (grade) => grade.value >= 5
+  ).length;
+
+  const failedGradesTotal: number = courseGrades.filter(
+    (grade) => grade.value < 5
+  ).length;
+
   const courseStats: CourseStats = {
     courseId: courseId,
     studentsCount: courseGrades.length,
-    passedCount: courseGrades.filter((grade) => grade.value >= 5).length,
-    passedCountPercentage:
-      (courseGrades.filter((grade) => grade.value >= 5).length * 100) /
-      courseGrades.length,
-    failedCount: courseGrades.filter((grade) => grade.value < 5).length,
-    failedCountPercentage:
-      (courseGrades.filter((grade) => grade.value < 5).length * 100) /
-      courseGrades.length,
+    passedCount: passedGradesTotal,
+    passedCountPercentage: (passedGradesTotal * 100) / courseGrades.length,
+    failedCount: failedGradesTotal,
+    failedCountPercentage: (failedGradesTotal * 100) / courseGrades.length,
     averageGrade:
       courseGradesValues.reduce(
         (totalValues, value) => totalValues + value,
         0
       ) / courseGradesValues.length,
-
     highestGrade: Math.max(...courseGradesValues),
     highestGradeStudentId: sortedByValueCourseGrades[0].studentId,
   };
+
   return courseStats;
 };
